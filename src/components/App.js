fedc5a9db455d65b9d9b32493de29fcf2aba0c9d
Marcos L <marcosrlj@outlook.com>
2021-10-21 15:46:53 -0300

    first commit
    initializing the bakesale app

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 40,
+  },
+});
+
+export default App;
