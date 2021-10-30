a3a64e406b2fda98a158aa1630bfb07f083a46ef
Marcos L <marcosrlj@outlook.com>
2021-10-30 02:22:35 -0300

    using the fetch api

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ajax from '../ajax';
+import DealList from './DealList';
 
 class App extends React.Component {
+  state = {
+    deals: [],
+  };
+
   async componentDidMount() {
     const deals = await ajax.fetchInitialDeals();
-
-    console.log('deals: ', deals);
+    // console.log('deals: ', deals);
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState(prevState => {
+      return { deals };
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>App</Text>
+        {this.state.deals.length > 0 ? (
+          <DealList deals={this.state.deals} />
+        ) : (
+          <Text style={styles.header}>App</Text>
+        )}
       </View>
     );
   }
